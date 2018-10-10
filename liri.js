a87c371d000dd6101a0e6c18104ae245938b5aae
memisgulden <memis@gmail.com>
2018-10-10 18:32:18 -0400

    omdb running

--- liri.js
+++ liri.js
@@ -2,27 +2,69 @@ require('dotenv').config();
 
 
 // Spotify API //
-var Spotify = require('node-spotify-api');
+// node liri.js spotify-this-song "song name" //
+if (process.argv[2] === "spotify-this-song") {
+    var Spotify = require('node-spotify-api');
 
-var spotify = new Spotify({
-    id: process.env.SPOTIFY_ID,
-    secret: process.env.SPOTIFY_SECRET,
-});
-
-var userInput = process.argv[3];
-
-spotify
-    .search({ type: 'track', query: userInput, limit: 1})
-    .then(function (response) {
-        console.log("----------------------");
-        console.log("Artist Name: " + response.tracks.items[0].album.artists[0].name);
-        console.log("Track: " + response.tracks.items[0].name);
-        console.log("Spotify Link: " + response.tracks.items[0].external_urls.spotify);
-        console.log("Album: " + response.tracks.items[0].album.name);
-        console.log("----------------------");
-    })
-    .catch(function (err) {
-        console.error('Error occurred: ' + err);
+    var spotify = new Spotify({
+        id: process.env.SPOTIFY_ID,
+        secret: process.env.SPOTIFY_SECRET,
     });
 
+    var userInput = process.argv[3];
 
+    spotify
+        .search({ type: 'track', query: userInput, limit: 1 })
+        .then(function (response) {
+            console.log("----------------------");
+            console.log("Artist Name: " + response.tracks.items[0].album.artists[0].name);
+            console.log("Track: " + response.tracks.items[0].name);
+            console.log("Spotify Link: " + response.tracks.items[0].external_urls.spotify);
+            console.log("Album: " + response.tracks.items[0].album.name);
+            console.log("----------------------");
+        })
+        .catch(function (err) {
+            console.error('Error occurred: ' + err);
+        });
+
+}
+// Bands in Town API //
+// node liri.js concert-this <artist/band name here> //
+// name of venue, venue location, date // 
+
+// var userInput = process.argv[3];
+
+// const request = require('request');
+
+// request("https://rest.bandsintown.com/artists/" + userInput
+//     + "/events?app_id=codingbootcamp", { json: true }, function (err, response, body) {
+//         if (err) { return console.log(err); }
+//         console.log(response._events.data);
+//     });
+
+// OMDB API //
+//node liri.js movie-this '<movie name here>' //
+if (process.argv[2] === "movie-this") {
+    var request = require("request");
+
+    var userInput = process.argv[3];
+
+
+    request("http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
+
+        if (!error && response.statusCode === 200) {
+
+            console.log("--------------------------------------");
+            console.log("Movie Title: " + JSON.parse(body).Title);
+            console.log("Year: " + JSON.parse(body).Year);
+            console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+            console.log("Country Produced: " + JSON.parse(body).Country);
+            console.log("Language: " + JSON.parse(body).Language);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
+            console.log("--------------------------------------");
+
+        }
+    })
+}
